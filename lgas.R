c36478c1a75389082131654c1ca805ca1cdf768b
Michael Lydeamore <michael.lydeamore@monash.edu>
2021-09-30 11:24:06 +1000

    Update WP2 analysis files

--- lgas.R
+++ lgas.R
@@ -48,7 +48,8 @@ lgas <- c(
 
 lgairsad <- fread("data/lga_irsad.csv") %>% janitor::clean_names()
 lgas <- unique(lgairsad$lga)
-lgas <- lgas[lgas %in% abs_lga_lookup$lga][!lgas %like% "Unincorp"]
+lgas <- lgas[lgas %in% abs_lga_lookup$lga]
+lgas <- lgas[!lgas %like% "Unincorp"]
 
 lga_ngms_unscaled <- tibble(
   lga = lgas
@@ -116,13 +117,7 @@ lga_TPs <- lga_ngms_filter %>%
 # plan modelling report
 
 tpframe <- data.table(tp=lga_TPs, lga=names(lga_TPs))
-lgairsad_simplified <- lgairsad[count > 0 & (irsad_deciles_at_lga_level_area != "Total" &
-                        irsad_deciles_at_lga_level_area != "Not applicable" &
-                        lga != "Total"), .(lga, decile=as.numeric(gsub("Decile ", "", irsad_deciles_at_lga_level_area)))]
 
-tpirsad <- merge(tpframe, lgairsad_simplified, by='lga', all.x=T)
-
-ggplot(tpirsad, aes(x=decile, y=tp)) + geom_point()
 
 lgas_care_about <- c(
   # NSW SEIFA 1
@@ -136,5 +131,63 @@ lgas_care_about <- c(
   #SA
   "Playford (C)",
   "Burnside (C)",
-  "Alice Springs (T)"
+  "Alice Springs (T)",
+  "Maribyrnong (C)",
+  "Canterbury-Bankstown (A)",
+  "Brimbank (C)"
 )
+
+
+
+# apply vaccination
+
+vacc_reductions <- lapply(seq_along(lga_ngms_filter), function(i) {
+  l <- lga_ngms_filter[[i]]
+  readRDS("data/vacc_effect_by_age_scenario_19.RDS") %>%
+    group_by(vacc_coverage, vacc_schoolkids) %>%
+    summarise(
+      tp_reduction=vacc_tp_reduction(vacc_effect, l)  
+    ) %>%
+    pivot_longer(
+      ends_with("reduction"),
+      names_to = "population_group",
+      values_to = "tp_multiplier"
+    ) %>%
+    mutate(
+      population_group = str_remove(population_group, "_tp_reduction"),
+      tp_percent_reduction = 100 * (1 - tp_multiplier)
+    ) %>%
+    arrange(
+      vacc_schoolkids,
+      vacc_coverage,
+      population_group
+    ) %>%
+    filter(
+      vacc_coverage == 0.8 & vacc_schoolkids
+    ) %>%
+    mutate(
+      lga=names(lga_ngms_filter)[i]
+    )
+}) %>% rbindlist()
+
+
+tps_vax <- tpframe %>% 
+  left_join(vacc_reductions, by='lga') %>%
+  mutate(tp_vax = tp * tp_multiplier) %>%
+  left_join(lgairsad_simplified, by='lga') %>%
+  left_join(conmat::abs_lga_lookup, by='lga')
+
+tps_vax[, label := paste0(state, "-", decile)]
+
+ggplot(tps_vax[lga %in% lgas_care_about], aes(y=label))+
+  geom_segment(aes(x=tp, xend=tp_vax, yend=label), arrow=arrow(length=unit(0.1, "inches"))) +
+  geom_point(aes(x=tp), colour="blue") +
+  geom_point(aes(x=tp_vax), colour="green3") +
+  #scale_y_discrete(labels=tps_vax[lga %in% lgas_care_about, label]) +
+  labs(x="Transmission potential", y="LGA") +
+  theme_bw() +
+  xlim(c(0, NA)) +
+  geom_vline(aes(xintercept=1), linetype="dashed") +
+  coord_flip()
+
+cowplot::save_plot(filename="plots/specific_lgas.png", last_plot())
--- states.R
+++ states.R
@@ -1,6 +1,8 @@
 # everything else is in lgas.R now, I wasn't sure where these fitted
 
-state_tps <- state_ngms %>%
+lapply(list.files("R/*.R", full.names = T), source)
+
+state_tps <- state_ngm %>%
   vapply(
     get_R,
     FUN.VALUE = numeric(1)
@@ -13,3 +15,31 @@ state_tps <- tibble(
 
 merged <- tpirsad %>% left_join(conmat::abs_pop_age_lga_2020 %>% select(state, lga) %>% unique(), by='lga') %>%
   left_join(state_tps, by='state') %>% as.data.table()
+
+
+ggplot(merged, aes(x=decile, y=relative_diff)) +
+  geom_point() + 
+  geom_hline(aes(yintercept=0), linetype="dashed") +
+  facet_wrap(~state) +
+  theme_bw() +
+  labs(x="SEIFA Decile", y="Transmission Potential")
+
+cowplot::save_plot(plot=last_plot(),
+                   filename="plots/tp_lga_by_state.png")
+
+
+merged2 <- merged %>% left_join(b[, .(lga=lga_name_2018, is_metro)], by='lga')
+
+labels <- c("Metro", "Non-Metro",
+            state_tps$state)
+names(labels) <- c("TRUE", "FALSE", state_tps$state)
+
+ggplot(merged2[!is.na(is_metro)], aes(x=decile, y=tp)) +
+  geom_point() + 
+  geom_hline(aes(yintercept=state_tp), linetype="dashed") + 
+  geom_hline(aes(yintercept=3.6), linetype="dotted", colour="red") +
+  facet_grid(state~is_metro, labeller = as_labeller(labels)) +
+  theme_bw() +
+  labs(x="SEIFA Decile", y="Transmission Potential") +
+  geom_text_repel(data=merged2[lga %in% lgas_care_about,], aes(label=lga)) +
+  geom_point(data=merged2[lga %in% lgas_care_about,], colour="red")
